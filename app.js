61de23d8efa2166e19ad7125c1e0c4f08c14dc29
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2021-09-23 00:43:45 -0300

    Addicting LocalStorage functions

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var arr = new Array();
 
 function adicionarItem() {
-    var item = document.getElementById("item").value;
-    var deadline = document.getElementById("deadline").value;
-    
+    let item = document.getElementById("item").value;
+    let deadline = document.getElementById("deadline").value;
+
     if(item === '' || deadline.value == ''){
         alert('Error message.');
     }else{
-        var lista  = document.getElementById("lista");
         
+        var lista  = document.getElementById("lista");
         var row = lista.insertRow(0);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
@@ -21,26 +21,20 @@ function adicionarItem() {
         cell3.innerHTML = deadline;
         cell4.innerHTML = mostraData();
         cell5.innerHTML = "<input type='button' value='X' onclick='deletarItem(item)'></input>";
-        
-        adicionarBanco();
+
+        salvarDadosLocalStorage(item, deadline);
         mostraLista();
         limpaLista();
     }  
 }
 
-
-function adicionarBanco(){
-    console.log("Adicionar itens ao banco local")
-}
-
 function mostraLista(){
-    console.log("Mostrar itens da lista");
+
 }
 
 function limpaLista(){
     document.getElementById('item').value='';
     document.getElementById('deadline').value='';
-
 }
 
 function mostraData(){
@@ -54,5 +48,53 @@ function mostraData(){
 }
 
 function deletarItem(item){
-    document.getElementById("lista").deleteRow(item.row)
+	let arrayTasks = JSON.parse(window.localStorage.getItem('tasks'));
+    console.log(item.value)
+    for(var i = 0; i< arrayTasks.length; i++){
+        if(arrayTasks[i].description === item){
+            arrayTasks.splice(arrayTasks[i], 1)
+        }
+    }
+    
+    arrayTasks.splice('item', 1);
+    
+    console.log(arrayTasks)
+    
+    document.getElementById("lista").deleteRow(item.row);
 }
+
+function salvarDadosLocalStorage(item, data){
+	let arrayTasks = JSON.parse(window.localStorage.getItem('tasks'));
+
+	if(!arrayTasks) arrayTasks = []; //Inicializando o vetor se ele vier vazio do local Storage
+
+	let objTask = {description: "", deadline: ""};
+	objTask.description = item;
+	objTask.deadline = data;
+
+	arrayTasks.push(objTask);
+	window.localStorage.setItem('tasks', JSON.stringify(arrayTasks));
+}
+
+function recuperarDadosLocalStorage(){
+	let arrayTasks = JSON.parse(window.localStorage.getItem('tasks'));
+
+	for(task of arrayTasks){
+        var lista  = document.getElementById("lista");
+        var row = lista.insertRow(0);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+
+        cell1.innerHTML = "<input type='checkbox'>";
+        cell2.innerHTML = task.description;
+        cell3.innerHTML = task.deadline;
+        cell4.innerHTML = "mostradata";
+        cell5.innerHTML = "<input type='button' value='X' onclick='deletarItem(item)'></input>";
+	}
+}
+
+
+recuperarDadosLocalStorage();
